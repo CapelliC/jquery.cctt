29861d2725ecc39864de7cfdcc8633c993db4c0a
CapelliC <cc.carlo.cap@gmail.com>
2017-11-22 18:10:00 +0100

    cleaned up

--- jquery.cctt.js
+++ jquery.cctt.js
@@ -47,7 +47,6 @@
 
         // get each sub TR on a TR branch
         row_tree: function(TR) {
-trace('row_tree', this, TR)
             var TD = TR.children('.cctt-control')
             if (TD.length === 1) {
                 // level = index of TD.control (should exists)
@@ -72,14 +71,11 @@ trace('row_tree', this, TR)
         },
 
         structure_layout: function() {
-            trace('structure_layout')
             var pk = []
             $(this).find('tr').each((i, v) => {
                 var r = $(v).children('td').map((j, u) => {
-                    trace('j,u', j,u)
                     return $(u).text()
                 }).toArray()
-                trace('r', r, pk)
                 if (pk.length) {
                 }
                 else
@@ -96,77 +92,17 @@ trace('row_tree', this, TR)
             // use event delegation to deal with toggling classes
             // suggestion by http://stackoverflow.com/a/16547457/874024
             TBODY.on('click', '.cctt-control', function(e) {
-
                 if (methods.settings.stop_propagation)
                     e.stopPropagation()
 
                 methods.toggle_node($(this))
-                /*
-                var TD = $(this)
-                var TR = $(TD.parent())
-
-                // action = on expanded ? hide : show
-                var ac = TR.is('.cctt-expanded') ? state.hide : state.show
-                var a0 = ac
-                
-                // level = index of TD.control (should exists)
-                var ic = TD.index()
-
-                // keep a stack
-                var st = [{action: ac, level: ic}]
-                function top() { return st[st.length - 1] }
-
-                // foreach sibling RS
-                TR.nextAll().each(function() {
-                    var RS = $(this)
-
-                    // on a control RS ?
-                    var CS = RS.children('.cctt-control')
-                    if (CS.length === 1) {
-                        
-                        // bail out ASAP
-                        var csi = CS.index()
-                        if (csi <= ic)
-                            return false    // we're done
-
-                        // get the current level status
-                        while (top().level >= csi)
-                            st.pop()
-                        
-                        ac = top().action
-                        if (RS.is('.cctt-collapsed')) {
-                            // assume related rows already set
-                            st.push({action: state.skip, level: csi})
-                        }
-                        else {
-                            // hide/show are clearly asymmetric
-                            if (a0 === state.hide)
-                                st.push({action: a0, level: csi})
-                            else
-                                st.push({action: ac, level: csi})
-                        }
-                    }
-                    
-                    // apply current action to current sibling
-                    if (ac === state.hide)
-                        RS.hide()
-                    else if (ac === state.show)
-                        RS.show()
-
-                    // next sibling processing
-                    ac = top().action
-                })
-                
-                TR.toggleClass('cctt-expanded cctt-collapsed')
-                */
             })
         },
 
         // 
         toggle_node: function(TD) {
-//trace('toggle_node', TD)
+
             var state = { skip: 0, hide: 1, show: 2 }
-            
             var TR = $(TD.parent())
 
             // action = on expanded ? hide : show
@@ -236,7 +172,6 @@ trace('row_tree', this, TR)
         // expand required branches to make element $el visible
         ensure_visible : function($el) {
             const $ps = methods.find_parents($el).reverse()
-//trace('ensure_visible', $el, $ps)
             $ps.forEach($tr => {
                 if ($tr.is(k_col))
                     methods.toggle_node(methods.find_control_td($tr))
@@ -272,37 +207,6 @@ trace('row_tree', this, TR)
 
         // utility currency
         // from https://stackoverflow.com/a/29347112/874024
-        /*
-        parsePotentiallyGroupedFloat : function(value) {
-            if (typeof value === 'string') {
-                var result = (value = value.trim()).replace(/[^0-9]/g, '')
-                if (/[,\.]\d{2}$/.test(value))
-                    result = result.replace(/(\d{2})$/, '.$1')
-                return parseFloat(result)
-            }
-            return parseFloat(value)
-        },
-        
-        currency: function(field) {
-            var s = $(field).val().trim()
-            if (s.length > 0)
-                return $().cctt('parsePotentiallyGroupedFloat', s)
-            return 0;
-        },
-        
-        fmtcurrency: function(c) {
-            var R = $().cctt('parsePotentiallyGroupedFloat', c).toLocaleString('it-IT', {maximumFractionDigits:2, minimumFractionDigits:2})
-            return R
-        },
-        euros: function(field) {
-            var c = $().cctt('currency', field)
-            var f = $().cctt('fmtcurrency', c)
-            return f + " €"
-        },
-        setcurrency: function(field, c) {
-            $(field).val($().cctt('fmtcurrency', c))
-        },
-        */
         parsePotentiallyGroupedFloat : function(value) {
             if (typeof value === 'string') {
                 var result = (value = value.trim()).replace(/[^0-9]/g, '')
@@ -337,7 +241,6 @@ trace('row_tree', this, TR)
     
     // standard dispatch of method
     $.fn.cctt = function(method) {
-        //trace('$.fn.cctt')
         if (methods[method]) {
             if (arguments.length == 1)  // 
                 return methods[method]
@@ -349,7 +252,7 @@ trace('row_tree', this, TR)
     }
 
     $.fn.cctt.defaults = {
-        // stop propagation on expand/collapse event
+        // stop event propagation on expand/collapse event
         stop_propagation: true,
     }
     
